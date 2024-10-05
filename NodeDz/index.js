bfb6ac04981d59034712b16537174ef13fe69236
Serhii <serhiiderkach1995@gmail.com>
2024-10-04 21:05:59 -0500

    01-node-homework

---
+++ NodeDz/index.js
@@ -0,0 +1,16 @@
+const arr = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr.length; i++) {
+
+    console.log(arr[i]);
+
+}
+
+function recursiveIteration(array, index = 0) { 
+    if (index === array.length) {
+        return;
+    }
+
+    console.log(array[index])
+    recursiveIteratuin(array, index + 1)
+}
